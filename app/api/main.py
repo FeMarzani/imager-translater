e9da18855a2ed613fa773e48a128855d0904c040
FeMarzani <marzaniarrieche@gmail.com>
2024-02-10 17:06:54 -0300

    :sparkles: feat: finalizando a implementação de uma API Flask do tipo POST para realizar o upload de uma imagem e retornar um áudio traduzido em português de uma imagem em qualquer idioma

--- app/api/main.py
+++ app/api/main.py
@@ -2,7 +2,19 @@
 import requests
 from pathlib import Path
 from openai import OpenAI
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
 
+# Cliente OpenAI
+client = OpenAI(api_key="COLOCAR CHAVE DA API AQUI")
+
+app = Flask(__name__)
+
+CORS(app)
+
+# Definindo funções:
+
+# Função para extrair texto de imagem.
 def text_extract(arqu_path):
 
     # Definindo URL da API para extrair texto de imagem
@@ -35,12 +47,10 @@ def text_extract(arqu_path):
 
     return texto_formatado
 
+# Função para realizar a tradução do texto retirado da imagem.
 def text_translate(texto_formatado):
 
-    # Cliente OpenAI
-    client = OpenAI(api_key="COLOCAR CHAVE DA API AQUI")
-
-    messages = client.chat.completions.create(
+    texto_traduzido = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "user", "content": f'Traduza a seguinte frase para o português: {texto_formatado}'}
@@ -48,6 +58,47 @@ def text_translate(texto_formatado):
     )
 
     # Retorna o resumo gerado
-    messages = messages.choices[0].message.content
+    texto_traduzido = texto_traduzido.choices[0].message.content
 
-    return messages
+    return texto_traduzido
+
+# Criando uma rota de API do tipo POST
+@app.route('/traducao', methods=['POST'])
+def speech():
+
+    # Verificando se há uma imagem na requisição enviada.
+    if 'image' not in request.files:
+        return jsonify({'error': 'Nenhuma imagem enviada'}), 400
+
+    image = request.files['image']
+
+    # Verificando o nome do arquivo corretamente
+    if image.filename == '':
+        return jsonify({'error': 'Nome do arquivo vazio'}), 400
+
+    # Salvando temporariamente o arquivo em um caminho
+    arqu_path = 'temp_image.jpg'
+    image.save(arqu_path)
+
+    # Extraindo texto da imagem.
+    texto_formatado = text_extract(arqu_path)
+
+    # Traduzindo texto da imagem.
+    texto_traduzido = text_translate(texto_formatado)
+
+    # Realizando agora o text to speech
+    speech_file_path = Path(__file__).parent / "speech.mp3"
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=texto_traduzido
+    )
+
+    response.stream_to_file(speech_file_path)
+
+    return send_file(speech_file_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
