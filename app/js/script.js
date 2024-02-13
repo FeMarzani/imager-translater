94fbce8e575e227e297dd3315dd236e9a4f560b5
FeMarzani <marzaniarrieche@gmail.com>
2024-02-13 11:39:11 -0300

    :sparkles: feat: adicionando evento na seleçaõ de imagem para demonstrar que uma imagem foi selecionada

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,10 @@
+function exibirNomeArquivo(input) {
+    if (input.files && input.files[0]) {
+        var nomeArquivo = input.files[0].name;
+        document.getElementById('nome-arquivo').innerHTML = 'Arquivo selecionado: ' + nomeArquivo;
+    }
+};
+
 document.querySelector('form').addEventListener('submit', function() {
     document.getElementById('retorno').style.display = 'block';
 });
